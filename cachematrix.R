b02c5c4002e3134c52a8649362119aa5f8fa812d
Timothy Butterfield <tsbutter@users.noreply.github.com>
2015-08-21 18:02:29 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a 'special matrix,' which is really a list.
 
-## Write a short comment describing this function
+## cacheSolve searches the cache for the matrix inverse; if the inverse is found, 
+##      the values are returned, if the values are not found, the values are computed.
+
+## Install MASS (Modern Applied Statistics with S) package: 
+        library(MASS)
+        ## ginv is a MASS command to compute the "Generalized Inverse of a Matrix." 
+        ##      See ?ginv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get, 
+             setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return (i)
+        }
+        data <- x$get()
+        i <- ginv(data, ...)
+        x$setinverse(i)
+        i
+}     
